88ca518025f85713bf0a13947435f28bf24a0e5d
Artyom Karpenko <azkarpenko@cloud.ru>
2024-01-14 18:31:16 +0300

    Задача 4 - добавили варианты реализации с циклом for и foreach

--- lesson02/Task04/Program.cs
+++ lesson02/Task04/Program.cs
@@ -3,12 +3,32 @@ int n = 6;
 int[] array = { 11, 2, 4, 7, 3, 9 };
 int i = 0;
 int max = array[0];
-while(i < n)
+while (i < n)
 {
     if (array[i] > max)
     {
         max = array[i];
     }
     i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{ 
+    if (e > max)
+    {
+        max = e;
+    }
 }
 Console.WriteLine(max);
