f27f91413786129b4af76c051ca100af33176bb0
Artyom Karpenko <azkarpenko@cloud.ru>
2024-01-14 18:03:19 +0300

    Задача 4 - Поиск максимального значения в массиве

---
+++ lesson02/Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Array;
+int n = 6;
+int[] array = { 11, 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
