6f79ed73418bc322082b1d55225188c3fb3edbf1
Artyom Karpenko <azkarpenko@cloud.ru>
2024-01-15 13:21:11 +0300

    Лекция 3 - Задача 2 - Вывод на экран квадратов чисел от 1 до N

---
+++ lesson03/Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести на экран квадратов числе от 1 до N
+void PrintSqares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSqares(5);
+PrintSqares(10);
+PrintSqares(15);
