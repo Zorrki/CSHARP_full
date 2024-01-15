dfa5e2c7081d3808ddcd7aef8dcb4015849d41d0
Artyom Karpenko <azkarpenko@cloud.ru>
2024-01-15 09:45:45 +0300

    Лекция 3 - Добавлена Задача 1

---
+++ lesson03/Task01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вычислить значение (axb)/(c+d) где a,b,c,d целые числа
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    double denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
