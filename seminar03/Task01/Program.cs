be2198a275dd65a65f9abdbeaa32282608604cd7
Artyom Karpenko <azkarpenko@cloud.ru>
2024-01-19 05:42:44 +0300

    Семинар 3 - Задача 1 + Задача 2 добавлены

--- seminar03/Task01/Program.cs
+++ seminar03/Task01/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Задать массив и написать программу которая определяет, присутствует ли заданное число в массиве
+// Ответ да/нет
+// Например [1 3 4 19 3] , 8 => нет ИЛИ 3 => да
+
+// тип_данных[] имя_массива = ...
+int[] array = { 11, 22, 33, 44, 55, 66, 77 };
+Console.Write("Введите число для поиска: ");
+int numberForSearch = Convert.ToInt32(Console.ReadLine());
+//bool isFound = false; //Число в массиве еще не найдено
+string isFound = "не найден";
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (numberForSearch == array [i]) // если число например 33 то сравниваем каждое число в массиве и проверяем
+    {
+        //isFound = true; // число найдено
+        isFound = "найден";
+        break; // ломает цикл как только найден элемент массива
+    }
+}
+    Console.Write($"Элемент {numberForSearch}: {isFound}");
+
+// if (isFound == true)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
---
+++ seminar03/Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задать массив из 10 элементов, заполненный числами из промежутка [-10, 10]
+// Заменить отрицательные элементы на положительные и наоборот
+
+int[] array = { -1, -2, -3, -4, -5, 6, 7, 8, 9, 10 };
+Console.WriteLine($"Изначальный массив: [{string.Join("; ", array)} ]");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1; // array[i] * -1
+    //Console.Write(array[i] + "\t");
+}
+Console.WriteLine($"=====================");
+Console.WriteLine($"Итоговый массив: [{string.Join("; ", array)} ]");
