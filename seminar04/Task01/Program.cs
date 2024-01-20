6f311a9131a151e90706e0c18936bef2ad6b5fd3
Artyom Karpenko <azkarpenko@cloud.ru>
2024-01-20 03:02:23 +0300

    4 семинар - Добавили Таск 1 и Таск 2

---
+++ seminar04/Task01/Program.cs
@@ -0,0 +1,33 @@
+﻿// тип_метода (void или нет) ИмяМетода (параметры)
+// char - 1 символ; string - массив char-ов
+// str = "923" , str[0] = 9, str[1] = 2, str[3] = 3
+
+// Простой калькулятор
+int Calculate(int a, int b, char sign)
+{
+    int res = 0;
+    if(sign == '+') // только для 1 символа
+    {
+        res = a + b;
+    }
+    else if (sign == '-')
+    {
+        res = a - b;
+    }
+    else if (sign == '*')
+    {
+        res = a * b;
+    }
+    else if (sign == '/')
+    {
+        res = a / b;
+    }
+    return res;
+}
+
+// Вызов метода: ИмяМетода(параметр1, параметр2, параметр3)
+Console.WriteLine(Calculate(10, 20, '+'));
+Console.WriteLine(Calculate(10, 20, '-'));
+Console.WriteLine(Calculate(10, 20, '*'));
+Console.WriteLine(Calculate(10, 20, '/'));
+Console.WriteLine(Calculate(10, 20, '.'));
---
+++ seminar04/Task02/Program.cs
@@ -0,0 +1,36 @@
+﻿// задать массив из N случайных целых чисел (вводится с клавиатуры)
+// Найти количество чисел, которые оканчиваются на 1 и делятся нацело на 7
+// [1 5 11 21 81 4 0 91 2 3]
+
+int[] FillArray(int size, int minRange, int maxRange)
+{
+    int[] arr = new int[size]; // Массив на size элементов
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minRange, maxRange + 1);
+        // Next (0,2) => 0,1 (правая граница исключена)
+    }
+    return arr;
+}
+
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(sizeArray, 0, 100);
+// Размер массива: sizeArray, элементы: [0,100]
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+
+int GetCountOfInterestingElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 7 == 0 && array[i] % 10 == 1)
+        // Число делится нацело на 7 и оканчивается на 1
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество чисел: {GetCountOfInterestingElements(array)}");
