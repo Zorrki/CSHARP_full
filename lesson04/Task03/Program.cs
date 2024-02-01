6d18cca42c72bf836c4c3a1ce6285f2f18c37fa6
Artem Karpenko <karartjom@gmail.com>
2024-02-01 14:27:36 +0300

    Лекция 4 - Задача 3 по строкам

---
+++ lesson04/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считать с консоли строку из цифр и латинских букв
+// Сформировать новую строку из букв исходной строки
+
+string GetLettersFromString (string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
